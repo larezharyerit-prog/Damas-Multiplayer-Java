df074dead07bf7927d52efc33aa79334d036c953
Haryerit Larez <larezharyerit@gmail.com>
2026-06-15 14:01:54 -0400

    Estructura Inicial de Paquetes

---
+++ DamasMultiplayer/src/damasmultiplayer/DamasMultiplayer.java
@@ -0,0 +1,11 @@
+
+package damasmultiplayer;
+
+public class DamasMultiplayer {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ DamasMultiplayer/src/interfaz/Interfaz.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaz;
+
+/**
+ *
+ * @author HARYERIT LAREZ
+ */
+public interface Interfaz {
+    
+}
